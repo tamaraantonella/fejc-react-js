68d2caa1e95e55b8f53c5b97a08ab2c5628a8c96
Jorge Luis Arteaga Almaraz <jorgitoaral@gmail.com>
2022-11-23 00:24:17 -0400

    feat: Add React Query

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import '@fontsource/montserrat';
 import '@fontsource/open-sans';
+import { QueryClient } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query/src/QueryClientProvider';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
@@ -7,9 +9,12 @@ import './index.css';
 import routes from './routes.jsx';
 
 const router = createBrowserRouter(routes);
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}/>
+    </QueryClientProvider>
   </React.StrictMode>
 );
