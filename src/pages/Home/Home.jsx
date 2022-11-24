40ac81cd4f1b01ce0063c014a2615ca6ea3a35a8
Jorge Luis Arteaga Almaraz <jorgitoaral@gmail.com>
2022-11-24 14:43:30 -0400

    feat: Add Home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+export const Home = () => {
+  return (
+    <div className="page">
+      <h2 className="title-2">
+        Mr. Van G
+      </h2>
+    </div>
+  );
+};
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import PhoneScreen from './components/PhoneScreen/index.js';
+import { Home } from './pages/Home/Home.jsx';
 import PaintingDetails from './pages/PaintingDetails/index.js';
 
 export default [
@@ -10,7 +11,7 @@ export default [
     children: [
       {
         path: '/',
-        element: <span>Home!</span>
+        element: <Home/>
       },
       {
         path: 'paintings/:paintingId',
