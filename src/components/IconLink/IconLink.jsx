b76d15cbfe8714a76ba8cea0e5f8426cbf586897
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-26 09:54:09 -0300

    iconLink component & tailwind config

--- src/components/IconLink/IconLink.jsx
+++ src/components/IconLink/IconLink.jsx
@@ -1,5 +1,5 @@
-import { ReactNode } from 'react';
-import { Link } from 'react-router-dom';
+import { ReactNode } from "react";
+import { Link } from "react-router-dom";
 
 /**
  *
@@ -9,12 +9,18 @@ import { Link } from 'react-router-dom';
  * @returns {JSX.Element}
  * @constructor
  */
-export const IconLink = ({
-  icon,
-  label,
-  to
-}) => {
+export const IconLink = ({ icon, label, to }) => {
   return (
-    <><Link to={to}>{label}</Link></>
+    <>
+      <Link
+        to={to}
+        className=" w-[151px] h-[180px] bg-black p-[10px] flex flex-col items-center gap-[10px] rounded-md"
+      >
+        <div className="w-[131px] h-[131px] flex grow ">{icon}</div>
+        <p className="text-artwork_text whitespace-nowrap text-ellipsis overflow-hidden">
+          {label}
+        </p>
+      </Link>
+    </>
   );
 };
