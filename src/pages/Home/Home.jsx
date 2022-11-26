ac9c3f44eae0e948fb19df2fb88b9a1d51c16a06
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-25 22:47:46 -0300

    home - import queries

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+import { useAllPaintings } from "../../api/useAllPaintinsg";
+import { useCategories } from "../../api/useCategories";
+
 export const Home = () => {
+  const { data } = useAllPaintings();
+  const getCategories = useCategories(data);
+  const categories = getCategories?.data?.categories;
+  const pictures = getCategories?.data?.pictures;
+
   return (
     <div className="page">
       <h2 className="title-2">
