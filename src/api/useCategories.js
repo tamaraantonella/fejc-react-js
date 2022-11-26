86d7987922371fb6135d2ef0faf29d2445d3b115
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-26 10:36:21 -0300

    simplify conditional

--- src/api/useCategories.js
+++ src/api/useCategories.js
@@ -5,24 +5,17 @@ import { useQuery } from '@tanstack/react-query';
 const getCategories = (paintings) => {
   const pictures = [];
   paintings?.forEach((painting) => {
-    pictures?.push(painting.category_ids);
+    pictures?.push(painting?.category_ids);
   })
   const result = new Set(pictures.flat());
   const unique = [...result];
   const finalIds = []
-  let uniqueIndex=0
+  let uniqueIndex = 0
   for (let index = 0; index < paintings?.length; index++)
   {
-    if (paintings[index]?.category_ids.length > 1 && paintings[index]?.category_ids.includes(unique[uniqueIndex]) && !finalIds?.includes(paintings[index]?.image_id))
-    {
-      finalIds?.push(paintings[index]?.image_id)
-      uniqueIndex++
-    }
-    if (paintings[index]?.category_ids.length === 1 && !finalIds?.includes(paintings[index]?.image_id) && paintings[index]?.category_ids.includes(unique[uniqueIndex]))
-    {
-      finalIds?.push(paintings[index]?.image_id)
-      uniqueIndex++
-    }
+    (paintings[index]?.category_ids?.includes(unique[uniqueIndex]) && !finalIds?.includes(paintings[index]?.image_id))
+      && (finalIds?.push(paintings[index]?.image_id),uniqueIndex++
+    )
   }
 
   return fetch(`https://api.artic.edu/api/v1/category-terms?ids=` + unique.join(','))
