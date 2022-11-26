34ae091035a60c7c995af61aa76200d88a8fbb2f
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-26 20:16:17 -0300

    fix: rename useAllPaintings and classname

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useAllPaintings } from "../../api/useAllPaintinsg";
+import { useAllPaintings } from "../../api/useAllPaintings";
 import { useCategories } from "../../api/useCategories";
 import { Accordion } from "../../components/Accordion/Accordion";
 import { IconLink } from "../../components/IconLink/IconLink";
@@ -18,41 +18,43 @@ export const Home = () => {
     <div className="page">
       <h2 className="title-2">Mr. Van G</h2>
       <div className=" flex flex-col items-center gap-[31px]">
-        {categories?.length > 1 && categories?.map((category, index) => (
-          <Accordion
-            title={category.title}
-            icon={
-              <img
-                className="h-full w-full object-cover"
-                src={`https://www.artic.edu/iiif/2/${pictures[index]}/full/50,/0/default.jpg`}
-              ></img>
-            }
-            expanded={expanded[category.id]}
-            subtitle={category.description}
-            onChange={handleChange.bind(null, category.id)}
-            className=" px-4 py-5 flex flex-col box-border bg-accordion_bg rounded-[10px]"
-            key={category.id}
-          >
-            {data
-              ?.filter((artwork) =>
-                artwork.category_ids.includes(categories[index].id)
-              )
-              .map((item) => (
-
-                  <IconLink
-                    key={item.id}
-                    label={item.title}
-                    icon={
-                      <img
-                        className="h-full w-full object-cover"
-                        src={`https://www.artic.edu/iiif/2/${item.image_id}/full/300,/0/default.jpg`}
-                      ></img>
-                    }
-                  to={`/paintings/${item.id}`}
+        {categories?.length > 1 &&
+          categories?.map((category, index) => (
+            <Accordion
+              title={category.title}
+              icon={
+                <img
+                  className="h-full w-full object-cover"
+                  src={`https://www.artic.edu/iiif/2/${pictures[index]}/full/50,/0/default.jpg`}
+                ></img>
+              }
+              expanded={expanded[category.id]}
+              subtitle={category.description}
+              onChange={handleChange.bind(null, category.id)}
+              className=" px-4 py-5 flex flex-col  bg-accordion_bg rounded-[10px]"
+              key={category.id}
+            >
+              <div className="w-full grid grid-cols-2 gap-[14px] py-4">
+                {data
+                  ?.filter((artwork) =>
+                    artwork.category_ids.includes(categories[index].id)
+                  )
+                  .map((item) => (
+                    <IconLink
+                      key={item.id}
+                      label={item.title}
+                      icon={
+                        <img
+                          className="h-full w-full object-cover"
+                          src={`https://www.artic.edu/iiif/2/${item.image_id}/full/300,/0/default.jpg`}
+                        ></img>
+                      }
+                      to={`/paintings/${item.id}`}
                     />
-              ))}
-          </Accordion>
-        ))}
+                  ))}
+              </div>
+            </Accordion>
+          ))}
       </div>
     </div>
   );
